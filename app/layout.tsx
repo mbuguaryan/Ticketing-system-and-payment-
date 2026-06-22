f5decebedd0e97f102f1dff0ed990b9b1ad1451c
mbugua <mbuguaryan3506@gmail.com>
2026-06-22 23:42:30 +0300

    Add Meta Pixel base tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+
+const META_PIXEL_ID = "3521405174667843";
 
 export const metadata: Metadata = {
   title: "Men Conference Nairobi 2026 Tickets",
@@ -17,6 +20,30 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         }}
       >
         {children}
+
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
